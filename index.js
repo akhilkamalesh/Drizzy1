28f66742c413d7310cbce96cf2b7249348cc8794
Akhil Kamalesh <akhilkamalesh@Akhils-MacBook-Pro.local>
2023-10-18 16:52:04 -0400

    Creation of the route for sofargone and the updating html router links

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ var path = require('path');
 var app = express(); // setting app to express
 
 app.use(express.static(path.join(__dirname, 'images'))); // static files for images to render in html
-app.use(express.static(path.join(__dirname, 'pages'))); // static files to access html files without path
-
 
 
 // Get request
 app.get('/', function(req, res){
-    res.sendFile('index.html', {title: 'Welcome'}); // can only render views
+    res.sendFile(path.join(__dirname,'pages/index.html'), {title: 'Welcome'}); // can only render views
                              // Can set parameters to pass to the view   
-})
+});
 
 // Get request
 app.get('/rankedalbums', function(req, res){
-    res.sendFile('index.html', {title: 'Welcome'}); // can only render views
+    res.sendFile(path.join(__dirname,'pages/rankedalbums.html'), {title: 'Welcome'}); // can only render views
                              // Can set parameters to pass to the view   
-})
+});
+
+// Get request
+app.get('/sofargone', function(req, res){
+    res.sendFile(path.join(__dirname,'pages/sofargone.html'), {title: 'Welcome'}); // can only render views
+                             // Can set parameters to pass to the view   
+});
 
 app.listen(3000);
 console.log('Server is running on port 3000')
