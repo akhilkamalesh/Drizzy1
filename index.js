62cd6a2805cf4c2bee7b421d44c2dfe29f592a44
Akhil Kamalesh <akhilkamalesh@Akhils-MacBook-Pro.local>
2023-10-18 22:19:25 -0400

    Database connection with .env file as well as db.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var path = require('path');
+var dotenv = require('dotenv');
+var connectDB = require('./config/db');
+
+// Load the environment variables
+dotenv.config({path: './config/config.env'});
+
+connectDB(); // Connect to the database via the method created in db.js
 
 var app = express(); // setting app to express
 
@@ -24,5 +31,11 @@ app.get('/sofargone', function(req, res){
                              // Can set parameters to pass to the view   
 });
 
-app.listen(3000);
-console.log('Server is running on port 3000')
+const server = app.listen(process.env.PORT, console.log('Server is running on port 3000'));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejections', (err, promise) => {
+    console.log(`Unhandled Rejection: ${err.message}`)
+    // Close server and exit process
+    server.close(() => process.exit(1))
+});
